cab93b9dfae75d774ea1a4e3e7ca4d269ea3e503
DowlathBashaG <9671419+DowlathBashaG@users.noreply.github.com>
2025-04-14 08:13:54 +0530

    Add files via upload
    Initial Commit

---
+++ Transaction-Propagation-Isolation/spring-transaction/src/main/java/dowlath/io/handler/OrderHandler.java
@@ -0,0 +1,22 @@
+package dowlath.io.handler;
+
+import dowlath.io.entity.Order;
+import dowlath.io.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class OrderHandler {
+
+    private final OrderRepository orderRepository;
+
+    public OrderHandler(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public Order saveOrder(Order order){
+        return orderRepository.save(order);
+    }
+}
